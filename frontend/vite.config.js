20511cd01e271b20e71905fe54505f9b99036788
arvindverma8012 <arvindverma8012@gmail.com>
2025-04-06 15:14:42 +0530

    fixed typo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Hospital-Management-System"/', // Replace <repository-name> with your GitHub repo name
+  base: '/Hospital-Management-System/', // Replace <repository-name> with your GitHub repo name
   plugins: [react()],
 })
